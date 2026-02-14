23e505b0481142ffc14b94e0f92dc8639800bf75
0xBahalaNa <0xBahalaNa@protonmail.com>
2026-02-14 14:02:28 -0800

    feat(ch03): complete exercise 3-1 control families

---
+++ exercises/ch03/exercise_3_1.py
@@ -0,0 +1,7 @@
+families = ["AC", "AT", "AU", "CA", "CM", "CP", "IA", "IR", "MA", "MP",
+            "PE", "PL", "PM", "PS", "PT", "RA", "SA", "SC", "SI", "SR"]
+
+print(families[0])
+print(families[19])
+print(families[7])
+print(f"{families[6]} is critical for CJIS compliance.")
