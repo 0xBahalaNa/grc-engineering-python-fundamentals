a25cca42ea5d4fe4e1eed3468c72dfcc1ad65fd4
0xBahalaNa <0xBahalaNa@protonmail.com>
2026-02-15 11:31:29 -0800

    feat(ch04): complete exercise 4-1 control family report

---
+++ exercises/ch04/exercise_4_1.py
@@ -0,0 +1,5 @@
+families = ["AC", "AT", "AU", "CA", "CM", "CP", "IA", "IR", "MA", "MP",
+            "PE", "PL", "PM", "PS", "PT", "RA", "SA", "SC", "SI", "SR"]
+
+for family in families:
+    print(f"Control Family: {family}")
