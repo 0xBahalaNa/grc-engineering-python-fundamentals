5898d7cf16c82ba2bae24241b1cb38f088eef056
0xBahalaNa <0xBahalaNa@protonmail.com>
2026-02-19 14:00:46 -0800

    feat(ch06): complete exercise 6-6 severity map comprehension

---
+++ exercises/ch06/exercise_6_6.py
@@ -0,0 +1,12 @@
+severity_dictionary = {}
+
+severity_names = ["Critical", "High", "Medium", "Low", "Info"]
+severity_scores = [4, 3, 2, 1, 0]
+
+# Name -> Score
+severity_map = {severity_names[i]: severity_scores[i] for i in range(len(severity_names))}
+print(severity_map)
+
+# Score -> Name (reverse mapping)
+reverse_map = {score: name for name, score in severity_map.items()}
+print(reverse_map)
