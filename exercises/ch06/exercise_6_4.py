f5f5a4a940d7ab8dbc86590e00ee5e8955d6f6e0
0xBahalaNa <0xBahalaNa@protonmail.com>
2026-02-21 18:49:01 -0800

    feat(ch06): refactor exercise 6-4 to use book patterns

--- exercises/ch06/exercise_6_4.py
+++ exercises/ch06/exercise_6_4.py
@@ -8,10 +8,25 @@ for finding_control in finding_controls:
 
 print(finding_count)
 
-max_control = max(finding_count, key=finding_count.get)
-print(f"\nMost findings: {max_control} ({finding_count[max_control]})")
+max_control = None
+max_value = 0
 
-sorted_controls = sorted(finding_count, key=finding_count.get, reverse=True)
+for control in finding_count:
+    if finding_count[control] > max_value:
+        max_value = finding_count[control]
+        max_control = control
+
+print(f"\nMost findings: {max_control} ({max_value})")
+
+# --- Manual sort (selection sort, descending) ---
+sorted_controls = list(finding_count.keys())
+
+for i in range(len(sorted_controls)):
+    max_idx = i
+    for j in range(i + 1, len(sorted_controls)):
+        if finding_count[sorted_controls[j]] > finding_count[sorted_controls[max_idx]]:
+            max_idx = j
+    sorted_controls[i], sorted_controls[max_idx] = sorted_controls[max_idx], sorted_controls[i]
 
 for control in sorted_controls:
     print(f"    {control}: {finding_count[control]}")
